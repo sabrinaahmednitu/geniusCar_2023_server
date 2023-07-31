fb70479603128c1ed3fa8ebc4697920d2dec254e
sabrina <sabrinaahmednitu@gmail.com>
2023-07-31 23:53:42 +0600

    get all service

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json())
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fkjuk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+    
+    const serviceCollection = client.db('geniusCar2023').collection('carservices');
+
+    app.get('/services', async (req, res) => {
+      const query = {};
+      const result = await serviceCollection.find(query).toArray();
+      res.send(result);
+
+    })
+
+ 
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('running at 5000');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
