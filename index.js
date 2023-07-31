5581e125afd81f85dbca8192ebc9f2ed65991db6
sabrina <sabrinaahmednitu@gmail.com>
2023-08-01 02:30:39 +0600

    service post api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +24,7 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
+ 
 
 async function run() {
   try {
@@ -36,13 +37,21 @@ async function run() {
       const query = {};
       const result = await serviceCollection.find(query).toArray();
       res.send(result);
-
     })
 
 
-    app.get('/seervices/:id', async (req, res) => {
+    app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
-      
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/services', async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+
     })
 
  
@@ -52,6 +61,7 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
 
 
 
