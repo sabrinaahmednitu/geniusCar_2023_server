930b4a2f7f62c1ab588900379200bc4b29da9377
sabrina <sabrinaahmednitu@gmail.com>
2023-08-01 11:35:59 +0600

    delete api

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ async function run() {
 
     })
 
+
+    app.delete('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await serviceCollection.deleteOne(query)
+      res.send(result);
+    })
+
  
   } finally {
     // Ensures that the client will close when you finish/error
